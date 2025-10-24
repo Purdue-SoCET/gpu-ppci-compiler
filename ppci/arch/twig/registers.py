b25fe371e81192e38f67f5b1712c005c36ed7db4
pbantval3 <pbantval@purdue.edu>
2025-10-23 20:56:20 -0400

    register file for 64 registers

--- ppci/arch/twig/registers.py
+++ ppci/arch/twig/registers.py
@@ -4,8 +4,12 @@ from ..registers import Register, RegisterClass
 # pylint: disable=invalid-name
 
 
-class RiscvRegister(Register):
+class TwigRegister(Register):
     bitsize = 32
+
+    @classmethod
+    def from_num(cls, num):
+        return num2regmap[num]
 
     def __repr__(self):
         if self.is_colored:
@@ -15,15 +19,7 @@ class RiscvRegister(Register):
             return self.name
 
 
-class RiscvProgramCounterRegister(Register):
-    bitsize = 32
-
-
-class RiscvFRegister(Register):
-    bitsize = 32
-
-
-class RiscvCsrRegister(Register):
+class TwigProgramCounterRegister(Register):
     bitsize = 32
 
 
@@ -38,81 +34,73 @@ def register_range(a, b):
     return {get_register(n) for n in range(a.num, b.num + 1)}
 
 
-R0 = RiscvRegister("x0", num=0, aka=("zero",))
-LR = RiscvRegister("x1", num=1, aka=("ra",))
-SP = RiscvRegister("x2", num=2, aka=("sp",))
-R3 = RiscvRegister("x3", num=3, aka=("gp",))
-R4 = RiscvRegister("x4", num=4, aka=("tp",))
-R5 = RiscvRegister("x5", num=5, aka=("t0",))
-R6 = RiscvRegister("x6", num=6, aka=("t1",))
-R7 = RiscvRegister("x7", num=7, aka=("t2",))
-FP = RiscvRegister("x8", num=8, aka=("s0", "fp"))
-R9 = RiscvRegister("x9", num=9, aka=("s1",))
-R10 = RiscvRegister("x10", num=10, aka=("a0",))
-R11 = RiscvRegister("x11", num=11, aka=("a1",))
-R12 = RiscvRegister("x12", num=12, aka=("a2",))
-R13 = RiscvRegister("x13", num=13, aka=("a3",))
-R14 = RiscvRegister("x14", num=14, aka=("a4",))
-R15 = RiscvRegister("x15", num=15, aka=("a5",))
-R16 = RiscvRegister("x16", num=16, aka=("a6",))
-R17 = RiscvRegister("x17", num=17, aka=("a7",))
-R18 = RiscvRegister("x18", num=18, aka=("s2",))
-R19 = RiscvRegister("x19", num=19, aka=("s3",))
-R20 = RiscvRegister("x20", num=20, aka=("s4",))
-R21 = RiscvRegister("x21", num=21, aka=("s5",))
-R22 = RiscvRegister("x22", num=22, aka=("s6",))
-R23 = RiscvRegister("x23", num=23, aka=("s7",))
-R24 = RiscvRegister("x24", num=24, aka=("s8",))
-R25 = RiscvRegister("x25", num=25, aka=("s9",))
-R26 = RiscvRegister("x26", num=26, aka=("s10",))
-R27 = RiscvRegister("x27", num=27, aka=("s11",))
-R28 = RiscvRegister("x28", num=28, aka=("t3",))
-R29 = RiscvRegister("x29", num=29, aka=("t4",))
-R30 = RiscvRegister("x30", num=30, aka=("t5",))
-R31 = RiscvRegister("x31", num=31, aka=("t6",))
+R0 = TwigRegister("x0", num=0, aka=("zero",))
+LR = TwigRegister("x1", num=1, aka=("ra",))
+SP = TwigRegister("x2", num=2, aka=("sp",))
+R3 = TwigRegister("x3", num=3, aka=("gp",))
+R4 = TwigRegister("x4", num=4, aka=("tp",))
+R5 = TwigRegister("x5", num=5, aka=("t0",))
+R6 = TwigRegister("x6", num=6, aka=("t1",))
+R7 = TwigRegister("x7", num=7, aka=("t2",))
+FP = TwigRegister("x8", num=8, aka=("s0", "fp"))
+R9 = TwigRegister("x9", num=9, aka=("s1",))
+R10 = TwigRegister("x10", num=10, aka=("a0",))
+R11 = TwigRegister("x11", num=11, aka=("a1",))
+R12 = TwigRegister("x12", num=12, aka=("a2",))
+R13 = TwigRegister("x13", num=13, aka=("a3",))
+R14 = TwigRegister("x14", num=14, aka=("a4",))
+R15 = TwigRegister("x15", num=15, aka=("a5",))
+R16 = TwigRegister("x16", num=16, aka=("a6",))
+R17 = TwigRegister("x17", num=17, aka=("a7",))
+R18 = TwigRegister("x18", num=18, aka=("s2",))
+R19 = TwigRegister("x19", num=19, aka=("s3",))
+R20 = TwigRegister("x20", num=20, aka=("s4",))
+R21 = TwigRegister("x21", num=21, aka=("s5",))
+R22 = TwigRegister("x22", num=22, aka=("s6",))
+R23 = TwigRegister("x23", num=23, aka=("s7",))
+R24 = TwigRegister("x24", num=24, aka=("s8",))
+R25 = TwigRegister("x25", num=25, aka=("s9",))
+R26 = TwigRegister("x26", num=26, aka=("s10",))
+R27 = TwigRegister("x27", num=27, aka=("s11",))
+R28 = TwigRegister("x28", num=28, aka=("t3",))
+R29 = TwigRegister("x29", num=29, aka=("t4",))
+R30 = TwigRegister("x30", num=30, aka=("t5",))
+R31 = TwigRegister("x31", num=31, aka=("t6",))
+R32 = TwigRegister("x32", num=32, aka=("s12",))
+R33 = TwigRegister("x33", num=33, aka=("s13",))
+R34 = TwigRegister("x34", num=34, aka=("s14",))
+R35 = TwigRegister("x35", num=35, aka=("s15",))
+R36 = TwigRegister("x36", num=36, aka=("s16",))
+R37 = TwigRegister("x37", num=37, aka=("s17",))
+R38 = TwigRegister("x38", num=38, aka=("s18",))
+R39 = TwigRegister("x39", num=39, aka=("s19",))
+R40 = TwigRegister("x40", num=40, aka=("s20",))
+R41 = TwigRegister("x41", num=41, aka=("s21",))
+R42 = TwigRegister("x42", num=42, aka=("s22",))
+R43 = TwigRegister("x43", num=43, aka=("s23",))
+R44 = TwigRegister("x44", num=44, aka=("s24",))
+R45 = TwigRegister("x45", num=45, aka=("s25",))
+R46 = TwigRegister("x46", num=46, aka=("s26",))
+R47 = TwigRegister("x47", num=47, aka=("s27",))
+R48 = TwigRegister("x48", num=48, aka=("s28",))
+R49 = TwigRegister("x49", num=49, aka=("s29",))
+R50 = TwigRegister("x50", num=50, aka=("s30",))
+R51 = TwigRegister("x51", num=51, aka=("s31",))
+R52 = TwigRegister("x52", num=52, aka=("s32",))
+R53 = TwigRegister("x53", num=53, aka=("s33",))
+R54 = TwigRegister("x54", num=54, aka=("s34",))
+R55 = TwigRegister("x55", num=55, aka=("s35",))
+R56 = TwigRegister("x56", num=56, aka=("s36",))
+R57 = TwigRegister("x57", num=57, aka=("s37",))
+R58 = TwigRegister("x58", num=58, aka=("s38",))
+R59 = TwigRegister("x59", num=59, aka=("s39",))
+R60 = TwigRegister("x60", num=60, aka=("s40",))
+R61 = TwigRegister("x61", num=61, aka=("s41",))
+R62 = TwigRegister("x62", num=62, aka=("s42",))
+R63 = TwigRegister("x63", num=63, aka=("s43",))
 
-PC = RiscvProgramCounterRegister("PC", num=32)
 
-F0 = RiscvFRegister("f0", num=0)
-F1 = RiscvFRegister("f1", num=1)
-F2 = RiscvFRegister("f2", num=2)
-F3 = RiscvFRegister("f3", num=3)
-F4 = RiscvFRegister("f4", num=4)
-F5 = RiscvFRegister("f5", num=5)
-F6 = RiscvFRegister("f6", num=6)
-F7 = RiscvFRegister("f7", num=7)
-F8 = RiscvFRegister("f8", num=8)
-F9 = RiscvFRegister("f9", num=9)
-F10 = RiscvFRegister("f10", num=10)
-F11 = RiscvFRegister("f11", num=11)
-F12 = RiscvFRegister("f12", num=12)
-F13 = RiscvFRegister("f13", num=13)
-F14 = RiscvFRegister("f14", num=14)
-F15 = RiscvFRegister("f15", num=15)
-F16 = RiscvFRegister("f16", num=16)
-F17 = RiscvFRegister("f17", num=17)
-F18 = RiscvFRegister("f18", num=18)
-F19 = RiscvFRegister("f19", num=19)
-F20 = RiscvFRegister("f20", num=20)
-F21 = RiscvFRegister("f21", num=21)
-F22 = RiscvFRegister("f22", num=22)
-F23 = RiscvFRegister("f23", num=23)
-F24 = RiscvFRegister("f24", num=24)
-F25 = RiscvFRegister("f25", num=25)
-F26 = RiscvFRegister("f26", num=26)
-F27 = RiscvFRegister("f27", num=27)
-F28 = RiscvFRegister("f28", num=28)
-F29 = RiscvFRegister("f29", num=29)
-F30 = RiscvFRegister("f30", num=30)
-F31 = RiscvFRegister("f31", num=31)
-
-MSTATUS = RiscvCsrRegister("mstatus", num=0x300)
-MIE = RiscvCsrRegister("mie", num=0x304)
-MTVEC = RiscvCsrRegister("mtvec", num=0x305)
-MEPC = RiscvCsrRegister("mepc", num=0x341)
-MCAUSE = RiscvCsrRegister("mcause", num=0x342)
-MHARTID = RiscvCsrRegister("mhartid", num=0xF14)
-FRM = RiscvCsrRegister("frm", num=0x2)
+PC = TwigProgramCounterRegister("PC", num=64)
 
 registers = [
     R0,
@@ -147,85 +135,80 @@ registers = [
     R29,
     R30,
     R31,
+    R32,
+    R33,
+    R34,
+    R35,
+    R36,
+    R37,
+    R38,
+    R39,
+    R40,
+    R41,
+    R42,
+    R43,
+    R44,
+    R45,
+    R46,
+    R47,
+    R48,
+    R49,
+    R50,
+    R51,
+    R52,
+    R53,
+    R54,
+    R55,
+    R56,
+    R57,
+    R58,
+    R59,
+    R60,
+    R61,
+    R62,
+    R63
 ]
-RiscvRegister.registers = registers
+TwigRegister.registers = registers
 
-fregisters = [
-    F0,
-    F1,
-    F2,
-    F3,
-    F4,
-    F5,
-    F6,
-    F7,
-    F8,
-    F9,
-    F10,
-    F11,
-    F12,
-    F13,
-    F14,
-    F15,
-    F16,
-    F17,
-    F18,
-    F19,
-    F20,
-    F21,
-    F22,
-    F23,
-    F24,
-    F25,
-    F26,
-    F27,
-    F28,
-    F29,
-    F30,
-    F31,
-]
-
-RiscvFRegister.registers = fregisters
 num2regmap = {r.num: r for r in registers}
 
 gdb_registers = registers + [PC]
-RiscvCsrRegister.registers = [MSTATUS, MIE, MTVEC, MEPC, MCAUSE, MHARTID, FRM]
 
-register_classes_hwfp = [
-    RegisterClass(
-        "reg",
-        [ir.i8, ir.i16, ir.i32, ir.ptr, ir.u8, ir.u16, ir.u32],
-        RiscvRegister,
-        [
-            R9,
-            R10,
-            R11,
-            R12,
-            R13,
-            R14,
-            R15,
-            R16,
-            R17,
-            R18,
-            R19,
-            R20,
-            R21,
-            R22,
-            R23,
-            R24,
-            R25,
-            R26,
-            R27,
-        ],
-    ),
-    RegisterClass("freg", [ir.f32, ir.f64], RiscvFRegister, fregisters),
-]
+# register_classes_hwfp = [
+#     RegisterClass(
+#         "reg",
+#         [ir.i8, ir.i16, ir.i32, ir.ptr, ir.u8, ir.u16, ir.u32],
+#         TwigRegister,
+#         [
+#             R9,
+#             R10,
+#             R11,
+#             R12,
+#             R13,
+#             R14,
+#             R15,
+#             R16,
+#             R17,
+#             R18,
+#             R19,
+#             R20,
+#             R21,
+#             R22,
+#             R23,
+#             R24,
+#             R25,
+#             R26,
+#             R27,
+#         ],
+#     ),
+#     RegisterClass("freg", [ir.f32, ir.f64], TwigFRegister, fregisters),
+# ]
 
 register_classes_swfp = [
     RegisterClass(
         "reg",
         [ir.i8, ir.i16, ir.i32, ir.ptr, ir.u8, ir.u16, ir.u32, ir.f32, ir.f64],
-        RiscvRegister,
+        TwigRegister,
         [
             R9,
             R10,
@@ -246,6 +229,38 @@ register_classes_swfp = [
             R25,
             R26,
             R27,
+            R32,
+            R33,
+            R34,
+            R35,
+            R36,
+            R37,
+            R38,
+            R39,
+            R40,
+            R41,
+            R42,
+            R43,
+            R44,
+            R45,
+            R46,
+            R47,
+            R48,
+            R49,
+            R50,
+            R51,
+            R52,
+            R53,
+            R54,
+            R55,
+            R56,
+            R57,
+            R58,
+            R59,
+            R60,
+            R61,
+            R62,
+            R63
         ],
     )
 ]
