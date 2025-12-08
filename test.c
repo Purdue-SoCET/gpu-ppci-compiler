53b0531ab5cd9effe177c9ccb5aaeb2e93a7348e
pbantval3 <pbantval@purdue.edu>
2025-12-07 21:22:54 -0500

    random test

--- test.c
+++ test.c
@@ -1,21 +1,22 @@
 //extern float cos(float);
-//extern int ftoi(float);
+// extern int ftoi(float);
 //extern float itof(int);
 //extern float sin(float);
 //extern float isqrt(float);
 
 int main() {
     int alpha = 3;
-    int beta;
-    if(alpha < 3){
-        beta = 4;
-    } else {
-        beta = 6;
+    int beta = 7;
+    int theta = 3;
+    while(beta != 0){
+        beta--;
+        theta = beta;
+        sin((float)beta);
+        while(theta != 0){
+            theta--;
+            alpha++;
+            cos((float)alpha);
+        }
     }
-    // int theta = 0;
-    // while(beta > 0){
-    //     beta--;
-    //     theta = theta * 2 + 1;
-    // }
-    return beta;
+    return alpha;
 }
