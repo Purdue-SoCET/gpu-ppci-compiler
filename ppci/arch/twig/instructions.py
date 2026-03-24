ec8139112248092152b68413b3285a73c61825e4
zhou1618 <54406733+jack41402@users.noreply.github.com>
2026-03-24 09:16:54 -0400

    Merge branch 'master' into test_merge

--- ppci/arch/twig/instructions.py
+++ ppci/arch/twig/instructions.py
@@ -349,12 +349,12 @@ class TwigPredLWInstruction(TwigInstruction):
 
 
 class Prsw(TwigPredSWInstruction):
-    """Store predicate register to memory: Mem[R[rs2] + imm] = P[prd]"""
+    """Store predicate register to memory: Mem[R[rs2] + imm] = P[prs]"""
 
     prs = Operand("prs", int)  # pred register index (source)
     rs2 = Operand("rs2", TwigRegister, read=True)  # base address GPR
     imm = Operand("imm", int)  # offset
-    syntax = Syntax(["prsw", " ", prs, ",", " ", rs2, ",", " ", imm])
+    syntax = Syntax(["prsw", " ", prs, ",", " ", imm, "(", rs2, ")"])
     patterns = {"opcode": 0b1101100, "prs": prs, "rs2": rs2, "imm": imm}
 
 
@@ -364,7 +364,7 @@ class Prlw(TwigPredLWInstruction):
     prd = Operand("prd", int)  # pred register index (dest)
     rs2 = Operand("rs2", TwigRegister, read=True)  # base address GPR
     imm = Operand("imm", int)  # offset
-    syntax = Syntax(["prlw", " ", prd, ",", " ", rs2, ",", " ", imm])
+    syntax = Syntax(["prlw", " ", prd, ",", " ", imm, "(", rs2, ")"])
     patterns = {"opcode": 0b1101101, "prd": prd, "rs2": rs2, "imm": imm}
 
 
